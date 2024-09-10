2da60c885a1fc508f55019e36c025b9a10eaa9ae
TheSaviourEking <saviour.eking@outlook.com>
2024-09-10 23:25:10 +0100

    chore: implement test 3 through 8

--- app/roman_numerals.py
+++ app/roman_numerals.py
@@ -1,2 +1,14 @@
 def parse(value):
-    return 1
+    value_dict = {
+        "I": 1,
+        "II": 2,
+        "III": 3,
+        "IV": 4,
+        "V": 5,
+        "VI": 6,
+        "VII": 7,
+        "VIII": 8,
+        "IX": 9
+    }
+
+    return value_dict[value] if value_dict[value] else None
