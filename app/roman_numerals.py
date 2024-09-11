598362bc5a107934a4c3c0714656f3e41e5e8a4b
TheSaviourEking <saviour.eking@outlook.com>
2024-09-11 01:09:41 +0100

    refactor: refactor my solution to the tests

--- app/roman_numerals.py
+++ app/roman_numerals.py
@@ -1,14 +1,31 @@
-def parse(value):
-    value_dict = {
+def parse(roman_numeral):
+    numerals_to_number_dict = {
         "I": 1,
-        "II": 2,
-        "III": 3,
         "IV": 4,
         "V": 5,
-        "VI": 6,
-        "VII": 7,
-        "VIII": 8,
-        "IX": 9
+        "IX": 9,
+        "X": 10,
+        "XL": 40,
+        "L": 50,
+        "XC": 90,
+        "C": 100,
+        "CD": 400,
+        "D": 500,
+        "CM": 900,
+        "M": 1000,
     }
 
-    return value_dict[value] if value_dict[value] else None
+    number = 0
+    i = 0
+
+    while i < len(roman_numeral):
+        if i + 1 < len(roman_numeral) and roman_numeral[i:i+2] in numerals_to_number_dict:
+            # If the next two characters form a valid Roman numeral
+            number += numerals_to_number_dict[roman_numeral[i:i+2]]
+            i += 2
+        else:
+            # else, it's a single character Roman numeral
+            number += numerals_to_number_dict[roman_numeral[i]]
+            i += 1
+
+    return number
--- test/test_roman_numerals_pytest.py
+++ test/test_roman_numerals_pytest.py
@@ -1,10 +1,15 @@
-import pytest
+# import pytest
 from pytest import mark
 from app.roman_numerals import parse
 
-@mark.parametrize('s, expected', [("IX", 9)])
+@mark.parametrize('s, expected', [
+    ("IX", 9), ('X', 10), ('XI', 11), ('XIV', 14), ('XIX', 19), 
+    ('XX', 20), ('XXXIV', 34), ('XLI', 41), ('L', 50), ('XCIX', 99),
+    ('C', 100), ('CCCXXXIII', 333), ('DLV', 555), ('CDXLIX', 449), ('MCMLXXII', 1972)
+    ])
 def test_roman_numeral_parser(s, expected):
     # Arrange
     value = parse(s)
+    #Act
     # Assert
     assert value == expected
